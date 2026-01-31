94088de378ff5c9a188dd0169ba76fe09b580537
Muhammadqodir779 <kadirovmuhammadqodir663@gmail.com>
2026-01-31 21:34:20 +0500

    workify_space_x

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,22 @@
-// import { createRoot } from 'react-dom/client';
-// import './index.css';
-// import App from './App.jsx';
-// import { BrowserRouter } from 'react-router-dom';
-
-// createRoot(document.getElementById('root')).render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// );
-// main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { createRoot } from 'react-dom/client';
 import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
+// main.jsx
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
